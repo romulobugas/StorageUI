fd6f0546cec91c2c61e34e9bfb6a79a8eaa58a59
romulomauriz <romulo.mauriz@mobweb.net.br>
2024-02-15 18:38:39 -0300

    Adição do PostgreSQL

---
+++ Models/UserApp.cs
@@ -0,0 +1,9 @@
+﻿namespace StorageUI.Models
+{
+    public class UserApp
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Password { get; set; }
+    }
+}
